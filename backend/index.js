d02293ea03121f9de1b65ce0498d2f97ed1b8611
Rasagna21105 <rasagnagollena@gmail.com>
2025-04-26 19:34:39 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ require('./Models/db');
 
 // CORS middleware - allow requests from frontend
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000'], // React default port - added both localhost and IP
+  origin: ['http://localhost:3000', 'http://127.0.0.1:3000','https://student-management-1-oil5.onrender.com'], // React default port - added both localhost and IP
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true // Allow credentials
